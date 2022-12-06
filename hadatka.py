1f8335682abb8af38520dfcf0f5e2885ecc97a51
kolokoc0 <kristiansvrlo@gmail.com>
2022-12-06 13:43:18 +0100

    hadatka

---
+++ hadatka.py
@@ -0,0 +1,28 @@
+subor = open('hada.txt','r',encoding="utf-8")
+subor2 = open('hada_komp.txt','w')
+def text (subor):
+    prvy_riadok = 0
+    poc = 0
+    vys = ""
+    poc2 = 1
+    for line in subor:
+        poc +=1
+        if len(line)>prvy_riadok:
+            prvy_riadok = len(line)
+        for i in range(1,len(line)):
+            if line[i-1] != line[i]:
+                subor2.write(line[i-1])
+                subor2.write(str(poc2))
+                poc2 = 1
+
+            else:
+                poc2 +=1
+            if line[i] == '\n':
+                subor2.write('\n')
+    if subor.readline() == "":
+        subor2.write(line[i-1])
+        subor2.write(str(poc2))
+    print('najdlhsia hra mala krokov:', (prvy_riadok))
+    print("zapisane hry:", poc)
+    print(subor2)
+print(text(subor))
